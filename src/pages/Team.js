8c740036a05f58cf85ba4b030568286cd256ce57
jsusilo <enr@bk.ru>
2021-11-20 00:01:45 +0500

    team page

---
+++ src/pages/Team.js
@@ -0,0 +1,5 @@
+      <header class="section page-header" data-component="Header">
+      <section class="section-xl section bg-white section-collapse" data-component="About">
+      <section class="section section-xl bg-white section-collapse" data-component="Content">
+      <section class="section-xl section bg-white section-collapse" data-component="Work">
+      <footer class="section footer-classic section-collapse" data-component="Footer">
